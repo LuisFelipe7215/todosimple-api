c0ffd67c45f3d1ad747b50fb72159487de6c9b19
LuisFelipe7215 <luis104.lf@gmail.com>
2024-07-17 09:46:15 -0300

    Create UserService

---
+++ src/main/java/com/luisfelipe/todosimple/services/UserService.java
@@ -0,0 +1,51 @@
+package com.luisfelipe.todosimple.services;
+
+import com.luisfelipe.todosimple.models.User;
+import com.luisfelipe.todosimple.repositories.TaskRepository;
+import com.luisfelipe.todosimple.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    public User findById(Long id) {
+        Optional<User> user = this.userRepository.findById(id);
+        return user.orElseThrow(() -> new RuntimeException("User cannot be found! id: " + id + ", Type: " + User.class.getName()));
+    }
+
+    @Transactional
+    public User create(User user) {
+        user.setId(null);
+        user = this.userRepository.save(user);
+        this.taskRepository.saveAll(user.getTasks());
+        return user;
+    }
+
+    @Transactional
+    public User update(User user) {
+        User newUser = findById(user.getId());
+        newUser.setPassword(user.getPassword());
+        return this.userRepository.save(newUser);
+    }
+
+    public void delete(Long id) {
+        findById(id);
+        try {
+            this.userRepository.deleteById(id);
+        } catch (Exception e) {
+            throw new RuntimeException("It's not possible to delete when there are related entities!");
+        }
+
+    }
+
+}
